e2c059351e045cd4973826157bd76861491ea05e
Marc <marc.baumann@ext.mercedes-benz.io>
2022-11-11 10:11:26 +0100

    made initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -7,8 +7,8 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     env: {
-      preprod_api: "https://shop-preprod.mercedes-benz.com/dcpconb2b-api/dcp-api/v2/",
-      base_store: "dcp-mbcb2b-de",
+      preprod_api: "https://shop-preprod.mercedes-benz.com/dcpconnect-eu/preprod/dcp-api/v2/",
+      base_store: "dcp-mbc-de",
       market: "de",
       lang: "de_DE"
     }
@@ -17,6 +17,6 @@ module.exports = defineConfig({
     user: "user",
     password: "Test1234!",
     host: "acidic-raspberries.db.elephantsql.com",
-    database: "testb2b",
+    database: "testb2c",
   }
 })
